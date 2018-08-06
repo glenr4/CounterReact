d08cb7a3c33133a5de95b6db9d32bee099bcb7c0
glenr4 <glenrutherford4@gmail.com>
2018-08-06 17:51:46 +1000

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import Counters from "./components/counters";
+import NavBar from "./components/navbar";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <span className="App-title">Welcome to React</span>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Counters />
+        </main>
+      </React.Fragment>
     );
   }
 }
